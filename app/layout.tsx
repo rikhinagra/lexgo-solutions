c0115755dc54689f5c8d2b60b9854197d527ad82
Chandan-devs-tech <chandanguptabsb@gmail.com>
2026-02-09 17:47:23 +0530

    Add Google Analytics (GA4) tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -101,6 +102,18 @@ export default function RootLayout({
         />
       </head>
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-NGHBB99FHQ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NGHBB99FHQ');
+          `}
+        </Script>
         {children}
       </body>
     </html>
