45bcb75b404560f5dbcd017b446a6e0925e84d72
Chandan-devs-tech <chandanguptabsb@gmail.com>
2026-03-09 19:10:17 +0530

    Add Google Tag Manager scripts to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,6 +94,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MQDSVHSC');`
+          }}
+        />
+        {/* End Google Tag Manager */}
         <meta name="theme-color" content="#0A1F44" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script
@@ -102,6 +113,16 @@ export default function RootLayout({
         />
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MQDSVHSC"
+            height="0" 
+            width="0" 
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-NGHBB99FHQ"
           strategy="afterInteractive"
